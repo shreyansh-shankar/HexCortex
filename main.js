2f0772a6f0500a17f76aecf8a6a9c8a0e242ad20
shreyansh-shankar <shreyanshshankar257@gmail.com>
2025-10-08 19:49:18 +0530

    Basic electron application setup

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'renderer.js'), // optional
+      nodeIntegration: true, // allow Node.js in renderer
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+
+  // Open DevTools (optional)
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
